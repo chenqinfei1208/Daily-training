ae00172de8da1d61dab0235aedbb2bd1206f85b9
chenqinfei1208 <1003750718@qq.com>
2020-12-20 22:03:22 +0800

    打印出生日期

---
+++ src/PrintBrith.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class PrintBrith {
+
+        public static void main(String[] args){
+            Scanner scanner = new Scanner(System.in);
+            String s = scanner.nextLine();
+            String year = s.substring(0, 4);
+            String month = s.substring(4, 6);
+            String days = s.substring(6, 8);
+            System.out.println("year="+year);
+            System.out.println("month="+month);
+            System.out.println("date="+days);
+
+        }
+
+    }
