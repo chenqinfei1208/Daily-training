1a279b97bee2e38aff7825e1e63fe18d29d92ca4
chenqinfei1208 <1003750718@qq.com>
2020-12-27 23:06:17 +0800

    例子

---
+++ src/Example.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Example {
+       public static void main(String args[]){
+           Scanner sc=new Scanner(System.in);
+             System.out.println("使用nextLine()方法，并且输入为：");
+             String n=sc.nextLine();
+             System.out.println("输出为：");
+             System.out.println(n);
+       }
+
+   }
