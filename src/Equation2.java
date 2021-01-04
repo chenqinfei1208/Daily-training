a9c5fb6095630278e06862332b32f9cf56c010a4
chenqinfei1208 <1003750718@qq.com>
2021-01-04 23:12:46 +0800

    计算一元二次方程

---
+++ src/Equation2.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Equation2 {
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            while(sc.hasNextDouble()){
+                double a=sc.nextDouble();
+                double b=sc.nextDouble();
+                double c=sc.nextDouble();
+                double d=b*b-4*a*c;
+                double e=Math.sqrt(d);
+                double x1, x2;
+                double s, x;
+                s=-b/(2*a);
+                x=Math.sqrt(-d)/(2*a);
+                if(a==0){
+                    System.out.println("Not quadratic equation");
+                }
+                else{
+                    if(d==0){
+                        x1=-b/(2*a);
+                        System.out.printf("x1=x2=%.2f\n", x1);
+                    }
+                    else if(d>0){
+                        x1=(b-e)/(-2*a);
+                        x2=(b+e)/(-2*a);
+                        System.out.printf("x1=%.2f",x1);
+                        System.out.print(";");
+                        System.out.printf("x2=%.2f",x2);
+                        System.out.print("\n");
+                    }
+                    else{
+                        System.out.printf("x1=%.2f", s);
+                        System.out.print("-");
+                        System.out.printf("%.2f", x);
+                        System.out.printf("i;x2=%.2f", s);
+                        System.out.print("+");
+                        System.out.printf("%.2f", x);
+                        System.out.print("i");
+                        System.out.print("\n");
+                    }
+                }
+
+            }
+        }
+
+    }
+    
