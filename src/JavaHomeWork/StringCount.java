2445ed9705f7c7f99927fddc5ca36dce80b1056a
chenqinfei1208 <1003750718@qq.com>
2021-03-06 23:34:11 +0800

    单词个数

---
+++ src/JavaHomeWork/StringCount.java
@@ -0,0 +1,16 @@
+package JavaHomeWork;
+
+public class StringCount {
+        public int lengthOfLastWord(String s) {
+            int length = 0;
+            for (int i = s.length() - 1; i >= 0; i--) {
+                if (s.charAt(i) != ' ') {
+                    length++;
+                } else if (length != 0) {
+                    return length;
+                }
+            }
+            return length;
+        }
+    }
+
