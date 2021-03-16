8852b8954a9c37f0ebd5b3369fbe40a542ab9022
chenqinfei1208 <1003750718@qq.com>
2021-03-16 23:20:48 +0800

    改为驼峰命名

---
+++ src/JavaHomeWork/NamingRule.java
@@ -0,0 +1,19 @@
+package JavaHomeWork;
+import java.util.Scanner;
+public class NamingRule {
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            while(sc.hasNext()){
+                String str = sc.nextLine();
+                String[] words = str.split("_");
+                String result = words[0];
+                for(int i=1;i<words.length;i++){
+                    result += (char)(words[i].charAt(0)-'a'+'A');
+                    for(int j=1;j<words[i].length();j++){
+                        result += words[i].charAt(j);
+                    }
+                }
+                System.out.println(result);
+            }
+        }
+    }
