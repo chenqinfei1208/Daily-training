870bff55666ab6008c70d46d122cf43412ad6f6b
chenqinfei1208 <1003750718@qq.com>
2021-04-21 23:27:58 +0800

    二叉树的深度

---
+++ src/DeepTreeNode.java
@@ -0,0 +1,36 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class DeepTreeNode {
+
+        public int maxDepth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            Queue<TreeNode> queue = new LinkedList<TreeNode>();
+            queue.offer(root);
+            int ans = 0;
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                while (size > 0) {
+                    TreeNode node = queue.poll();
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                    size--;
+                }
+                ans++;
+            }
+            return ans;
+        }
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    }
+
